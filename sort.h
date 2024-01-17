2c4118cf2e4b5ca4ed24b30cfcf37520a87e8d33
LNCarrion <lestycarrion@gmail.com>
2024-01-17 16:24:23 -0400

    fix

--- sort.h
+++ sort.h
@@ -13,6 +13,7 @@
  * @prev: Pointer to the previous element of the list
  * @next: Pointer to the next element of the list
  */
+/*structures*/
 typedef struct listint_s
 {
 	const int n;
@@ -29,9 +30,5 @@ void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 
-<<<<<<< HEAD
-#endif /* SORT_H */
-
-=======
 #endif /* SORT_H  */
->>>>>>> 893f249c89ff7f7338f2679b80c96af94e342226
+
